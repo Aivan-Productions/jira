6fb642648a443bc62b593854dbeb7b6b32972e5a
zenov2006 <zenovdonya@gmail.com>
2025-10-05 17:41:39 +0300

    fix:imports2

--- src/boards/models.py
+++ src/boards/models.py
@@ -1,9 +1,11 @@
+from sqlalchemy import Integer, String, ForeignKey, DateTime, func, Column
+from sqlalchemy.orm import relationship
 from src.database import Base
 class Boards(Base):
     __tablename__ = "boards"
-    id = Column(Integer, primary_key=True, autoincreament = True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(100), nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=func.mow())
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
-    author = relationship("Users", back_populates="author", nullable=False)
+    author = relationship("Users", back_populates="author")
     author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
