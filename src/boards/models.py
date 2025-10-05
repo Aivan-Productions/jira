f88a08c7264fe70308402b42f39f87bc236ed354
zenov2006 <zenovdonya@gmail.com>
2025-10-05 16:46:07 +0300

    fix: change imports

--- src/boards/models.py
+++ src/boards/models.py
@@ -1,15 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-import datetime
-engine = create_engine('sqlite:///database.db', echo=True)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-
-class Board(Base):
+from src.database import Base
+class boards(Base):
     __tablename__ = "Boards"
-    id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincreament = True)
+    title = Column(String(100), nullable=False)
     author = relationship("Users", back_populates="author", nullable=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime)
+    created_at = Column(DateTime, nullable=False, server_default=func.mow())
+    updated_at = пшеColumn(DateTime, server_default=func.mow(), onupdate=func.now())
