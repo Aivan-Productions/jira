18f3cca4dc575962da255ae1a7365390cee2c9b3
zenov2006 <zenovdonya@gmail.com>
2025-10-05 17:18:59 +0300

    fix:add one-to-many

--- src/boards/models.py
+++ src/boards/models.py
@@ -1,8 +1,9 @@
 from src.database import Base
-class boards(Base):
-    __tablename__ = "Boards"
+class Boards(Base):
+    __tablename__ = "boards"
     id = Column(Integer, primary_key=True, autoincreament = True)
     title = Column(String(100), nullable=False)
-    author = relationship("Users", back_populates="author", nullable=False)
     created_at = Column(DateTime, nullable=False, server_default=func.mow())
-    updated_at = пшеColumn(DateTime, server_default=func.mow(), onupdate=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    author = relationship("Users", back_populates="author", nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
